d592dfe05ae27b079053cb1a7d79b52a3e61412b
RafaMoreno <faelazomoreno@gmail.com>
2019-06-29 09:27:31 +0200

    Other commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'react_disasterbox/build')));
 
 app.use('/', indexRouter);
 
