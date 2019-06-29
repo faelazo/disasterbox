321dd0bd44bdb9ca49ac3ab9632ad823c1d39b17
RafaMoreno <faelazomoreno@gmail.com>
2019-06-29 10:03:01 +0200

    Other again

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(cors({origin: 'https://disasterbox.herokuapp.com/'}));
+app.use(cors({origin: 'https://disasterbox.herokuapp.com'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
